f925d82c2d39f6c3752a46869462c70a4358213b
renatoruiz2607 <renatoruiz2607@gmail.com>
2021-10-17 23:16:45 -0300

    iniciada tela de login

--- random-rango/random-rango/Modules/HomeViewController.swift
+++ random-rango/random-rango/Modules/HomeViewController.swift
@@ -11,5 +11,10 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+    }
+    
+    func setupUI(){
+        view.backgroundColor = UIColor(red: 0.96, green: 0.97, blue: 0.89, alpha: 1.00)
     }
 }
