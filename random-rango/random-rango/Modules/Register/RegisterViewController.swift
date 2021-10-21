1bf8a620ab8455d616989702918097222b0b4d13
luizclear <clemente10claro@gmail.com>
2021-10-20 22:01:52 -0300

    Criar Button

--- random-rango/random-rango/Modules/Register/RegisterViewController.swift
+++ random-rango/random-rango/Modules/Register/RegisterViewController.swift
@@ -11,19 +11,12 @@ class RegisterViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupUI()
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func setupUI() {
+        // criarCadastroButton.layer.borderColor = UIColor.clear.cgColor
+        view.backgroundColor = UIColor(red: 0.96, green: 0.97, blue: 0.89, alpha: 1.00)
     }
-    */
 
 }
