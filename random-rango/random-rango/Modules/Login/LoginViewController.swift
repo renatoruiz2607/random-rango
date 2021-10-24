b6fd8d8c8b245e002b7d2dede61e899ded081196
renatoruiz2607 <renatoruiz2607@gmail.com>
2021-10-24 19:54:01 -0300

    criada pasta e arquivos da pagina de login

---
+++ random-rango/random-rango/Modules/Login/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  random-rango
+//
+//  Created by Fleury on 24/10/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
