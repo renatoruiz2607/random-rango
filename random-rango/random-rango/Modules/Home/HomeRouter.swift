4e800ac619d754698b69d976bc9aa9ec35214c19
renatoruiz2607 <renatoruiz2607@gmail.com>
2021-10-25 12:08:49 -0300

    configurada scrollview e iniciado layout da tela

--- random-rango/random-rango/Modules/Home/HomeRouter.swift
+++ random-rango/random-rango/Modules/Home/HomeRouter.swift
@@ -18,7 +18,8 @@ class HomeRouter {
         case .login:
 //            let vm =
 //            let router =
-            let vc = ForgotPasswordViewController()
+            let vc = LoginViewController()
+//            let vc = ForgotPasswordViewController()
 //            vc.modalPresentationStyle = .fullScreen
             vc.view.backgroundColor = .white
             context.navigationController?.show(vc, sender: nil)
