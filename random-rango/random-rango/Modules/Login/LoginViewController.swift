d0bb8a15a5366d4d1eabdd6d8ce081440d0bce20
renatoruiz2607 <renatoruiz2607@gmail.com>
2021-10-26 15:20:22 -0300

    finalizado layout da nova tela de Login

--- random-rango/random-rango/Modules/Login/LoginViewController.swift
+++ random-rango/random-rango/Modules/Login/LoginViewController.swift
@@ -9,8 +9,52 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var logoImage: UIImageView!
+    @IBOutlet weak var separatorLabel: UILabel!
+    @IBOutlet weak var socialMediaFirstImage: UIImageView!
+    @IBOutlet weak var socialMediaSecondImage: UIImageView!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var forgotPassButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var scrollContentView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+    }
+    
+    func setupUI() {
+        view.backgroundColor = UIColor(red: 0.96, green: 0.97, blue: 0.89, alpha: 1.00)
+        scrollView.backgroundColor = UIColor(red: 0.96, green: 0.97, blue: 0.89, alpha: 1.00)
+        scrollContentView.backgroundColor = UIColor(red: 0.96, green: 0.97, blue: 0.89, alpha: 1.00)
+        separatorLabel.backgroundColor = UIColor(red: 0.96, green: 0.97, blue: 0.89, alpha: 1.00)
+        logoImage.image = UIImage(named: "appLogo")
+        socialMediaFirstImage.image = UIImage(named: "googleLogoG")
+        socialMediaSecondImage.image = UIImage(named: "facebookLogo")
+        forgotPassButton.tintColor = UIColor(red: 0.94, green: 0.59, blue: 0.37, alpha: 1.00)
+        signUpButton.tintColor = UIColor(red: 0.94, green: 0.59, blue: 0.37, alpha: 1.00)
+        
+        loginButton.tintColor = UIColor(red: 1.00, green: 0.95, blue: 0.74, alpha: 1.00)
+        loginButton.backgroundColor = UIColor(red: 0.88, green: 0.22, blue: 0.33, alpha: 1.00)
+        loginButton.layer.cornerRadius = 25.0
+        
+        textFieldUI(textFieldNeeded: emailTextField)
+        textFieldUI(textFieldNeeded: passwordTextField)
+        
+        let statusBarView = UIView(frame: UIApplication.shared.statusBarFrame)
+        let statusBarColor = UIColor(red: 0.96, green: 0.97, blue: 0.89, alpha: 1.00)
+        statusBarView.backgroundColor = statusBarColor
+        view.addSubview(statusBarView)
+    }
+    
+    func textFieldUI(textFieldNeeded: UITextField) {
+        textFieldNeeded.layer.cornerRadius = 25.0
+        textFieldNeeded.layer.borderWidth = 1
+        textFieldNeeded.layer.borderColor = UIColor(red: 0.96, green: 0.97, blue: 0.89, alpha: 1.00).cgColor
+        textFieldNeeded.clipsToBounds = true
     }
 
 }
